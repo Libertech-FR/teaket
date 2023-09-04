b8bf0227d3e56cb16dcb68db3f2f4fc403f4de00
Tacx <12997062+tacxou@users.noreply.github.com>
2023-09-04 20:37:58 +0200

    fix api syntax categories

--- service/src/core/categories/_schemas/categories.schema.ts
+++ service/src/core/categories/_schemas/categories.schema.ts
@@ -1,8 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document, Types } from 'mongoose'
+import { Types } from 'mongoose'
 import { AbstractSchema } from '~/_common/abstracts/schemas/abstract.schema'
-
-export type CategoriesDocument = Categories & Document
 
 @Schema({
   collection: 'categories',
@@ -33,12 +31,14 @@ export class Categories extends AbstractSchema {
   @Prop({
     required: true,
     type: Boolean,
+    default: true,
   })
   public selectable: boolean
 
   @Prop({
     required: true,
     type: Boolean,
+    default: false,
   })
   public disabled: boolean
 
