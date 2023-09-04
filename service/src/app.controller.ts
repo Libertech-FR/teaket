db399213132e04eb96b46cc47c8ef081d04ece6c
Tacx <12997062+tacxou@users.noreply.github.com>
2023-09-04 18:52:21 +0200

    WIP shutdown service

--- service/src/app.controller.ts
+++ service/src/app.controller.ts
@@ -3,16 +3,23 @@ import { AbstractController } from './_common/abstracts/abstract.controller'
 import { AppService } from './app.service'
 import { Response } from 'express'
 import { Public } from './_common/decorators/public.decorator'
+import { ShutdownService } from '~/shutdown.service'
 
 @Public()
 @Controller()
 export class AppController extends AbstractController {
-  public constructor(private readonly _service: AppService) {
+  public constructor(private readonly _service: AppService, private readonly _shutdown: ShutdownService) {
     super()
   }
 
   @Get()
   public root(@Res() res: Response): Response {
     return res.json(this._service.getInfos())
+  }
+
+  @Get('shutdown')
+  public shutdown(@Res() res: Response): Response {
+    this._shutdown.shutdown()
+    return res.json({})
   }
 }
