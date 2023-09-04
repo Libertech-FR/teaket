5710b9e8512433e5be385bd605fbd92178b23cab
Tacx <12997062+tacxou@users.noreply.github.com>
2023-09-04 17:13:49 +0200

    WIP source request schema

--- service/src/tickets/source-request/_schemas/source-request.schema.ts
+++ service/src/tickets/source-request/_schemas/source-request.schema.ts
@@ -1,13 +1,58 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { AbstractSchema } from '~/_common/abstracts/schemas/abstract.schema'
-
-export type SourceRequestDocument = SourceRequest & Document
+import { RulePart, RulePartSchema } from '~/tickets/source-request/_schemas/parts/rules.part.schema'
 
 @Schema({
   collection: 'source-requests',
   versionKey: false,
 })
-export class SourceRequest extends AbstractSchema {}
+export class SourceRequest extends AbstractSchema {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  public name: string
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  public description: string
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  public icon: string
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  public color: string
+
+  @Prop({
+    type: [RulePartSchema],
+    required: true,
+  })
+  public rules: RulePart[]
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  public backgroundColor: string
+
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  public order: number
+
+  @Prop({
+    type: Object,
+  })
+  public customFields: object
+}
 
 export const SourceRequestSchema = SchemaFactory.createForClass(SourceRequest)
