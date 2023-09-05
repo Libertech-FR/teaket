74d59a08d2c376bcbbc03c3a8fd52f82601f6839
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-05 16:58:02 +0200

    WIP Projects DTO

--- service/src/core/projects/_dto/project.dto.ts
+++ service/src/core/projects/_dto/project.dto.ts
@@ -1,7 +1,37 @@
-import { PartialType } from "@nestjs/swagger"
+import { IsString, IsOptional, IsDate, IsObject, IsMongoId } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { AbstractCustomFieldsDto } from '~/_common/abstracts/dto/abstract.custom-fields.dto'
 
-export class ProjectCreateDto {
+export class ProjectCreateDto extends AbstractCustomFieldsDto {
+  @IsString()
+  @ApiProperty()
+  public name: string
 
+  @IsDate()
+  @IsOptional()
+  @ApiProperty()
+  public startDate?: Date
+
+  @IsDate()
+  @IsOptional()
+  @ApiProperty()
+  public endDate?: Date
+
+  @IsObject()
+  @IsOptional()
+  @ApiProperty()
+  public rules?: { [key: string]: any }
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  public description?: string
 }
 
-export class ProjectUpdateDto extends PartialType(ProjectCreateDto) {}
+export class ProjectDto extends ProjectCreateDto {
+  @IsMongoId()
+  @ApiProperty()
+  public _id: string
+}
+
+export class ProjectUpdateDto extends ProjectCreateDto {}
