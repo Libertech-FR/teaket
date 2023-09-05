d9cc7b95b61cf5ea641c1492c8089763a263f5af
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-05 16:58:15 +0200

    WIP triggers DTO

--- service/src/core/triggers/_dto/triggers.dto.ts
+++ service/src/core/triggers/_dto/triggers.dto.ts
@@ -1,7 +1,34 @@
-import { PartialType } from "@nestjs/swagger"
+import { IsString, IsOptional, IsBoolean, IsObject, IsMongoId } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class TriggersCreateDto {
+  @IsString()
+  @ApiProperty()
+  public name: string
 
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  public description?: string
+
+  @IsObject()
+  @ApiProperty()
+  public actions: { [key: string]: any }
+
+  @IsOptional()
+  @ApiProperty()
+  public pluginId?: string
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty()
+  public disabled?: boolean
 }
 
-export class TriggersUpdateDto extends PartialType(TriggersCreateDto) {}
+export class TriggersDto extends TriggersCreateDto {
+  @IsMongoId()
+  @ApiProperty()
+  public _id: string
+}
+
+export class TriggersUpdateDto extends TriggersCreateDto {}
