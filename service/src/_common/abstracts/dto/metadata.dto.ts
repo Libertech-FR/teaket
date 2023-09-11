1f6f55bc1a9313f9459475008ff7c03d1eb8f1d0
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-11 17:49:55 +0200

    fix metadata schema name + optionnal

--- service/src/_common/abstracts/dto/metadata.dto.ts
+++ service/src/_common/abstracts/dto/metadata.dto.ts
@@ -1,10 +1,11 @@
-import { IsObject, ValidateNested } from 'class-validator'
+import { IsObject, ValidateNested, IsOptional } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import { MetadataPartDto } from './parts/metadata.part.dto'
 
 export class MetadataDto {
   @IsObject()
+  @IsOptional()
   @ValidateNested()
   @Type(() => MetadataPartDto)
   @ApiProperty({ type: MetadataPartDto })
