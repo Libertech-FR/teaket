ba7806f01a01b1e233b89df29f8a6f3b9a6f22b0
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-14 16:13:40 +0200

    WIP ticket page

---
+++ app/src/components/ticket/rightPanel.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+q-list
+    q-expansion-item(label="Enveloppe")
+        q-card 
+            q-card-section
+                q-input
+    q-expansion-item(label="Informations")
+        q-card 
+            q-card-section
+                q-input
+    q-expansion-item(label="Cycle de vie")
+        q-card 
+            q-card-section
+                q-input
+span {{ ticketData }}
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    ticketData: {
+        type: Object,
+        required: true
+    }
+})
+</script>
--- app/src/pages/ticket/[id].vue
+++ app/src/pages/ticket/[id].vue
@@ -1,15 +1,32 @@
 <template lang="pug">
-    
+.column
+    .row.q-gutter-lg.items-center
+        q-breadcrumbs 
+            q-breadcrumbs-el(to="/") Accueil
+            q-breadcrumbs-el(to="/tickets") Tickets
+            q-breadcrumbs-el(:label="ticketData.data.sequence")
+    .row.q-gutter-md.col-11
+        .col-2
+            tk-ticketLeftPanel
+        .col-6
+            tk-ticketMainPanel()
+        .col-3
+            tk-ticketRightPanel(:ticketData="ticketData.data")
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRoute } from 'nuxt/app';
+import { useHttpApi } from '~/composables/useHttpApi';
 
 const route = useRoute()
 const id = ref<string>('')
+const { data: ticketData } = await useHttpApi(`tickets/ticket/${route.params.id}`)
 
-onMounted(() => {
-    id.value = route.params.id
+onMounted(async () => {
+
 })
+
+
+
 </script>
