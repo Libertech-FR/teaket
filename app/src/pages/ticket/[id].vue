88a955a07c80e417c40927d6f973d907d87b0596
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-14 09:08:10 +0200

    Ajout vue ticket

---
+++ app/src/pages/ticket/[id].vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'nuxt/app';
+
+const route = useRoute()
+const id = ref<string>('')
+
+onMounted(() => {
+    id.value = route.params.id
+})
+</script>
