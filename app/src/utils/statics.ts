c6e02d1459ee49d225db6da89be88795c2c76f36
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-20 10:00:11 +0200

    add types & utils

--- app/src/utils/statics.ts
+++ app/src/utils/statics.ts
@@ -7,3 +7,11 @@ export const lifeSteps = [
   { label: 'Ouvert', value: 1, group: 'lifestep', icon: 'mdi-circle', color: 'green' },
   { label: 'Clos', value: 0, group: 'lifestep', icon: 'mdi-circle', color: 'red' },
 ]
+
+export enum ThreadType {
+  SYSTEM = 1,
+  INTERNAL = 2,
+  EXTERNAL = 3,
+  INCOMING = 4,
+  OUTGOING = 5,
+}
