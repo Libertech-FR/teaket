a7665fce589c0273fa0a024a2535c2153179ac42
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-09-20 10:01:45 +0200

    type ticket id as mongoid

--- service/src/tickets/thread/_dto/thread.dto.ts
+++ service/src/tickets/thread/_dto/thread.dto.ts
@@ -6,11 +6,12 @@ import { IdfsPartDto } from '~/_common/dto/parts/idfs.part.dto'
 import { IdnamePartDto } from '~/_common/dto/parts/idname.part.dto'
 import { ThreadType, ThreadTypeList } from '~/tickets/thread/_enum/thread-type.enum'
 import { MetadataDto } from '~/_common/abstracts/dto/metadata.dto'
+import { Types } from 'mongoose'
 
 export class ThreadCreateDto extends MetadataDto {
   @IsMongoId()
   @ApiProperty()
-  public ticketId: string
+  public ticketId: Types.ObjectId
 
   @IsNumber()
   @IsEnum(ThreadTypeList)
