50f496fd3bf4ca3fc1a269358f5c33766b33dc2b
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-10-11 16:47:19 +0200

    types fixs

--- app/src/utils/mongoId.ts
+++ app/src/utils/mongoId.ts
@@ -1,8 +1,15 @@
 import ObjectId from 'bson-objectid'
 
-export function generateMongoId(id: string = '') {
+export function generateMongoId(id: string = ''): ObjectId {
   if (id) {
     return new ObjectId(id)
   }
   return new ObjectId()
+}
+
+export function generateStringMongoId(id: string = ''): string {
+  if (id) {
+    return new ObjectId(id).toHexString()
+  }
+  return new ObjectId().toHexString()
 }
