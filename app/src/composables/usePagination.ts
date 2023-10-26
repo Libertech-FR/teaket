ce0f3c1e397904bb15bd09d72ede26328b0a8da9
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-10-26 15:44:32 +0200

    fix pagination + fix update lifestep top left

--- app/src/composables/usePagination.ts
+++ app/src/composables/usePagination.ts
@@ -6,13 +6,13 @@ export default function usePagination() {
   const route = useRoute()
   const router = useRouter()
   const defaultSortBy = 'metadata.lastUpdatedAt'
-  const defaultDescending = false
+  const defaultDescending = true
   const pagination = ref<QTableProps['pagination']>({
     rowsNumber: 0,
     page: 1,
     rowsPerPage: 10,
     sortBy: 'metadata.lastUpdatedAt',
-    descending: false,
+    descending: true,
   })
 
   function initializePagination(total: number = 0) {
@@ -25,6 +25,7 @@ export default function usePagination() {
     pagination.value.rowsNumber = total
 
     setSortOptions(query)
+    paginationQuery()
   }
 
   function setSortOptions(query: { [x: string]: LocationQueryValue | LocationQueryValue[] }) {
@@ -57,7 +58,7 @@ export default function usePagination() {
     const query = removeSortKey()
     const skip = `${(pagination.value.page - 1) * pagination.value.rowsPerPage}`
     const limit = `${pagination.value.rowsPerPage}`
-    let sortKey = defaultSortBy
+    let sortKey = `sort[${defaultSortBy}]`
     let sortDirection = defaultDescending ? 'desc' : 'asc'
     if (pagination.value.sortBy) {
       sortKey = `sort[${pagination.value.sortBy}]`
