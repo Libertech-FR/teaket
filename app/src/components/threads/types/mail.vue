81b1f46ff235d0861bca3591b982bf5c43ac3612
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-11-09 11:45:00 +0100

    clear console og

--- app/src/components/threads/types/mail.vue
+++ app/src/components/threads/types/mail.vue
@@ -1,37 +1,37 @@
 <template lang="pug">
 div
-    div(v-for="(fragment, fragId) in props.data.fragments", :key='fragId').row
-        q-chat-message(
-            :sent="props.data.metadata.createdBy === user.username"
-            :name="props.data.metadata.createdBy" size="12"
-        ).col-11
-            template(v-slot:stamp)
-                q-separator.q-my-xs
-                .row.items-center
-                    q-icon(name="mdi-email").q-mx-sm
-                    span {{ getTimeFrom(props.data.metadata.createdAt) }}
-                        q-tooltip.text-body2 {{ getHour(props.data.metadata.createdAt) }}
-            template(v-slot:default)
-                div
-                    div(v-for='(raw, key) in props.data.fragments.raw' :key='key' v-html="raw.message.data")
-                    object(
-                      v-for='(file, key) in props.data.fragments.file'
-                      :key='key'
-                      :ref="el => { objectIframe[file.filestorage.id] = el }"
-                      :data='"http://host.docker.internal:7100" + file.filestorage.link'
-                      width='100%'
-                      height='400px'
-                      style="background: white;"
-                    )
-                      pre.q-ma-none.q-pa-sm(style='color: red; background: white;')
-                        | Impossible d'afficher le contenu du mail&nbsp;
-                        a(:href='"http://host.docker.internal:7100" + file.filestorage.link' target='_blank') {{ file.filestorage.name || file.filestorage.id }}
-                    q-separator.q-my-xs(v-if="props.data.attachments.length > 0")
-                    a(v-for='(attachment, key) in props.data.attachments' :key='key' :href="'http://host.docker.internal:7100' + attachment.link" target="_blank")
-                      q-chip(icon="mdi-paperclip" text-color="white" color="primary" dense size='md' :label="attachment.name")
-        .col.flex.items-center.q-pa-sm.column.q-gutter-sm
-            q-btn(round size="md" icon="mdi-share" color="primary" @click="emailReponse(props.data)")
-            q-btn(round size="md" icon="mdi-dots-vertical" color="primary")
+  div(v-for="(fragment, fragId) in props.data.fragments", :key='fragId').row
+    q-chat-message(
+      :sent="props.data.metadata.createdBy === user.username"
+      :name="props.data.metadata.createdBy" size="12"
+    ).col-11
+      template(v-slot:stamp)
+        q-separator.q-my-xs
+        .row.items-center
+          q-icon(name="mdi-email").q-mx-sm
+          span {{ getTimeFrom(props.data.metadata.createdAt) }}
+            q-tooltip.text-body2 {{ getHour(props.data.metadata.createdAt) }}
+      template(v-slot:default)
+        div
+          div(v-for='(raw, key) in props.data.fragments.raw' :key='key' v-html="raw.message.data")
+          object(
+            v-for='(file, key) in props.data.fragments.file'
+            :key='key'
+            :ref="el => { objectIframe[file.filestorage.id] = el }"
+            :data='"http://host.docker.internal:7100" + file.filestorage.link'
+            width='100%'
+            height='400px'
+            style="background: white;"
+          )
+            pre.q-ma-none.q-pa-sm(style='color: red; background: white;')
+              | Impossible d'afficher le contenu du mail&nbsp;
+              a(:href='"http://host.docker.internal:7100" + file.filestorage.link' target='_blank') {{ file.filestorage.name || file.filestorage.id }}
+          q-separator.q-my-xs(v-if="props.data.attachments.length > 0")
+          a(v-for='(attachment, key) in props.data.attachments' :key='key' :href="'http://host.docker.internal:7100' + attachment.link" target="_blank")
+            q-chip(icon="mdi-paperclip" text-color="white" color="primary" dense size='md' :label="attachment.name")
+    .col.flex.items-center.q-pa-sm.column.q-gutter-sm
+      q-btn(round size="md" icon="mdi-share" color="primary" @click="emailReponse(props.data)")
+      q-btn(round size="md" icon="mdi-dots-vertical" color="primary")
 </template>
 
 <script lang="ts" setup>
@@ -70,7 +70,6 @@ const user = store.state.value.auth.user
 
 const emit = defineEmits(['email:response'])
 const emailReponse = async (thread: ThreadDto) => {
-  console.log('thread', thread)
   const { data, error } = await useHttpApi(`/core/filestorage/{_id}/raw`, {
     method: 'get',
     pathParams: {
@@ -80,7 +79,6 @@ const emailReponse = async (thread: ThreadDto) => {
       mime: 'message/partial',
     },
   })
-  console.log('emailReponse', data.value)
   emit('email:response', {
     ...thread.mailinfo,
     message: data?.value,
