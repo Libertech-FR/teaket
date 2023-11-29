647267d8de910f29b6bb097b46e4e93fa12db3d7
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-11-29 12:19:31 +0100

    Update tk-form-builder component in ticket page

--- app/src/pages/ticket/index.vue
+++ app/src/pages/ticket/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-tk-form-builder(:json="data.data")
+tk-form-builder(:json="data.data" :model-value="data2.data")
   template(#top-actions)
     q-btn(@click="refresh" label="refresh" icon="mdi-refresh" flat)
 </template>
@@ -13,6 +13,17 @@ const {
   method: 'get',
   pathParams: {
     _id: `65539756ed13b613ad02aa56`,
+  },
+})
+
+const {
+  data: data2,
+  refresh: refresh2,
+  error: error2,
+} = await useHttpApi(`/tickets/ticket/{_id}`, {
+  method: 'get',
+  pathParams: {
+    _id: `6560c79f45074e2f2285a2e2`,
   },
 })
 
